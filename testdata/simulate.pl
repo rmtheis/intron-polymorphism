4c75e5d19c05bf1c7ecfe67733655f7c53f300ad
Robert Theis <robert.m.theis@gmail.com>
2012-06-14 11:55:25 -0700

    save mutated reference

--- testdata/simulate.pl
+++ testdata/simulate.pl
@@ -19,7 +19,6 @@
 # along with Bowtie 2.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-
 #
 # The paired-end data is made by (a) changing to the reads subdirectory and (b)
 # running 'perl simulate.pl --ref=../reference/lambda_virus.fa'.
@@ -31,8 +30,6 @@
 # --unpaired --prefix=longreads'.
 #
 
-
-
 use strict;
 use warnings;
 use Carp;
@@ -40,19 +37,36 @@ use Math::Random qw(random_normal random_exponential);
 use Getopt::Long;
 use List::Util qw(max min);
 
-my @fa_fn = ( "testGenome.fna" );       # files with reference FASTA
-my $rf = "";          # reference sequence
-my $long = 0;         # 1 -> generate long reads
-my $paired = 1;       # 1 -> generate paired-end reads
-my $prefix = "reads"; # output files start with this string
+my @fa_fn = ();        # files with reference FASTA
+my $rf = "";           # reference sequence
+my $long = 0;          # 1 -> generate long reads
+my $paired = 1;        # 1 -> generate paired-end reads
+my $prefix = "reads";  # output files start with this string
+my $nreads    = undef; # # reads
+my $rdlen_av  = undef; # average to use when drawing from exponential
+my $rdlen_exact = undef; # exact length for all reads, overrides randomness
+my $rdlen_min = undef; # minimum read length (added to exponential draw)
+my $frag_av   = undef; # mean fragment len
+my $frag_sd   = undef; # s.d. to use when drawing frag len from normal dist
+my $verbose   = 0;     # be talkative
 
 GetOptions (
 	"fasta|reference=s" => \@fa_fn,
 	"long"              => \$long,
+	"verbose"           => \$verbose,
+	"nreads=i"          => \$nreads,
+	"read-avg=i"        => \$rdlen_av,
+	"read-len=i"        => \$rdlen_exact,
+	"read-min=i"        => \$rdlen_min,
+	"frag-avg=i"        => \$frag_av,
+	"frag-sd=i"         => \$frag_sd,
 	"unpaired"          => sub { $paired = 0; },
 	"prefix=s"          => \$prefix
 ) || die "Bad option";
 
+scalar(@fa_fn) > 0 || die "Must specify at least one reference FASTA file with --fasta";
+
+print STDERR "Loading reference files...\n";
 for my $fn (@fa_fn) {
 	open(FN, $fn) || confess;
 	my $name = "";
@@ -85,11 +99,11 @@ sub revcomp {
 	return $ret;
 }
 
-my $nreads    = 1000000;  # number of reads/end to generate
-my $rdlen_av  = 75;     # average read length to use when drawing from exponential
-my $rdlen_min = 40;     # minimum read length (added to exponential draw)
-my $frag_av   = 250;    # mean fragment len
-my $frag_sd   = 45;     # s.d. to use when drawing frag len from normal dist
+$nreads    = $nreads    || 10000; # number of reads/end to generate
+$rdlen_av  = $rdlen_av  || 75;    # average when drawing from exponential
+$rdlen_min = $rdlen_min || 40;    # min read length (added to exponential draw)
+$frag_av   = $frag_av   || 250;   # mean fragment len
+$frag_sd   = $frag_sd   || 45;    # s.d. when drawing frag len from normal dist
 my @fraglens  = ();     # fragment lengths (for paired)
 my @readlens  = ();     # read/end lengths
 
@@ -109,7 +123,7 @@ sub rand_dna($) {
 # Mutate the reference
 #
 
-# Add SNPs
+print STDERR "Adding single-base substitutions...\n";
 my $nsnp = 0;
 for(0..length($rf)-1) {
 	if(rand() < 0.0012) {
@@ -119,27 +133,43 @@ for(0..length($rf)-1) {
 	}
 }
 
-# Add microindels
-my $newrf = "";
+print STDERR "Adding microindels...\n";
 my $microgap = 0;
-for(0..length($rf)-1) {
-	$newrf .= substr($rf, $_, 1);
-	if(rand() < 0.0010) {
-		my $len = int(random_exponential(1, 3))+1;
-		if(int(rand()) == 0) {
-			# Remove a bit of the reference
-			$len = min($len, length($newrf));
-			$newrf = substr($newrf, 0, length($newrf)-$len);
-		} else {
-			# Add a bit to the reference
-			$newrf .= rand_dna($len);
-		}
-		$microgap++;
+{
+	my $newrf = "";
+	my $nins = int(length($rf) * 0.0005 + 0.5);
+	my $ndel = int(length($rf) * 0.0005 + 0.5);
+	$microgap = $nins + $ndel;
+	my %indel = ();
+	for(1..$nins) {
+		my $off = int(rand(length($rf)));
+		$indel{$off}{ty} = "ins";
+		$indel{$off}{len} = int(random_exponential(1, 3))+1;
 	}
+	for(1..$ndel) {
+		my $off = int(rand(length($rf)));
+		$indel{$off}{ty} = "del";
+		$indel{$off}{len} = int(random_exponential(1, 3))+1;
+	}
+	my $lasti = 0;
+	for my $rfi (sort {$a <=> $b} keys %indel) {
+		if($rfi > $lasti) {
+			$newrf .= substr($rf, $lasti, $rfi - $lasti);
+			$lasti = $rfi;
+		}
+		if($indel{$rfi}{ty} eq "ins") {
+			$newrf .= rand_dna($indel{$rfi}{len});
+		} else {
+			$lasti += $indel{$rfi}{len};
+		}
+	}
+	if($lasti < length($rf)-1) {
+		$newrf .= substr($rf, $lasti, length($rf) - $lasti - 1);
+	}
+	$rf = $newrf;
 }
-$rf = $newrf;
 
-# Add some larger rearrangements
+print STDERR "Adding larger rearrangements...\n";
 my $nrearr = int(random_exponential(1, 3)+1);
 for(0..$nrearr) {
 	my $break = int(rand(length($rf)));
@@ -153,20 +183,36 @@ print STDERR "Added $nsnp SNPs\n";
 print STDERR "Added $microgap Microindels\n";
 print STDERR "Added $nrearr Rearrangements\n";
 
+# Save the mutated reference
+open(RD, ">${prefix}_mutated_sequence.fa") || die;
+print RD ">Sequence_from_${prefix}-mutated_by_simulate.pl\n";
+for ( my $pos = 0; $pos < length($rf); $pos += 70 ) {
+  print RD substr($rf, $pos, 70), "\n";
+}
+close(RD);
+
+
 #
 # Simulate reads
 #
 
+print STDERR "Picking read and fragment lengths...\n";
+# Pick random read lengths
+if(defined($rdlen_exact)) {
+	@readlens = ($rdlen_exact) x ($nreads * ($paired ? 2 : 1));
+} else {
+	@readlens = random_exponential($nreads * ($paired ? 2 : 1), $rdlen_av);
+	@readlens = map int, @readlens;
+	@readlens = map { int($_ + $rdlen_min) } @readlens;
+}
 if($paired) {
 	# Pick random fragment and read lengths
 	@fraglens = random_normal($nreads, $frag_av, $frag_sd);
-	@readlens = random_exponential($nreads*2, $rdlen_av);
-} else {
-	# Pick random read lengths
-	@readlens = random_exponential($nreads, $rdlen_av);
+	@fraglens = map int, @fraglens;
+	for(my $i = 0; $i < scalar(@readlens); $i += 2) {
+		$fraglens[$i/2] = max($fraglens[$i/2], $readlens[$i] + $readlens[$i+1]);
+	}
 }
-@fraglens = map int, @fraglens;
-@readlens = map { int($_ + $rdlen_min) } @readlens;
 
 sub rand_quals($) {
 	my $ret = "";
@@ -180,6 +226,7 @@ sub rand_quals($) {
 
 sub add_seq_errs($$) {
 	my($rd, $qu) = @_;
+	my $origLen = length($rd);
 	for(0..length($rd)-1) {
 		my $c = substr($rd, $_, 1);
 		my $q = substr($qu, $_, 1);
@@ -191,19 +238,34 @@ sub add_seq_errs($$) {
 		substr($rd, $_, 1) = $c;
 		substr($qu, $_, 1) = $q;
 	}
+	length($rd) == $origLen || die;
 	return $rd;
 }
 
 # Now simulate 
+print STDERR "Simulating reads...\n";
 my $rflen = length($rf);
 if($paired) {
-	open(RD1, ">${prefix}.1.fq") || die;
-	open(RD2, ">${prefix}.2.fq") || die;
-	for my $i (0..$#fraglens) {
+	open(RD1, ">${prefix}_1.fq") || die;
+	open(RD2, ">${prefix}_2.fq") || die;
+	for(my $i = 0; $i < scalar(@fraglens); $i++) {
 		# Extract fragment
 		my $flen = $fraglens[$i];
 		my $off = int(rand($rflen - ($flen-1)));
 		my $fstr = substr($rf, $off, $flen);
+		# Check if it has too many Ns
+		my %ccnt = ();
+		for my $j (1..$flen) {
+			my $c = uc substr($fstr, $j, 1);
+			$ccnt{tot}++;
+			$ccnt{non_acgt}++ if ($c ne "A" && $c ne "C" && $c ne "G" && $c ne "T");
+			$ccnt{$c}++;
+		}
+		# Skip if it has >10% Ns
+		if(1.0 * $ccnt{non_acgt} / $ccnt{tot} > 0.10) {
+			$i--;
+			next;
+		}
 		# Possibly reverse complement
 		$fstr = revcomp($fstr) if (int(rand(2)) == 0);
 		# Get reads 1 and 2
@@ -227,20 +289,35 @@ if($paired) {
 	}
 	close(RD1);
 	close(RD2);
-	print STDERR "Made pairs: ${prefix}.1.fq/${prefix}.2.fq\n";
+	print STDERR "Made pairs: reads_1.fq/reads_2.fq\n";
 } else {
 	open(RD1, ">${prefix}.fq") || die;
-	for my $i (0..$#readlens) {
+	for(my $i = 0; $i < scalar(@readlens); $i++) {
 		# Extract fragment
 		my $rdlen = $readlens[$i];
 		my $off = int(rand($rflen - ($rdlen-1)));
 		my $rd = substr($rf, $off, $rdlen);
+		# Check if it has too many Ns
+		my %ccnt = ();
+		for my $j (1..$rdlen) {
+			my $c = uc substr($rd, $j, 1);
+			$ccnt{tot}++;
+			$ccnt{non_acgt}++ if ($c ne "A" && $c ne "C" && $c ne "G" && $c ne "T");
+			$ccnt{$c}++;
+		}
+		# Skip if it has >10% Ns
+		if(1.0 * $ccnt{non_acgt} / $ccnt{tot} > 0.10) {
+			$i--;
+			next;
+		}
+		length($rd) == $rdlen || die;
 		# Possibly reverse complement
 		$rd = revcomp($rd) if int(rand(2)) == 0;
 		# Generate random quality values
 		my $qu = rand_quals($rdlen);
+		length($rd) == length($qu) || die "length(seq) = ".length($rd).", length(qual) = ".length($qu);
 		$rd = add_seq_errs($rd, $qu);
-		length($rd) == length($qu) || die;
+		length($rd) == length($qu) || die "length(seq) = ".length($rd).", length(qual) = ".length($qu);
 		# Print
 		print RD1 "\@r".($i+1)."\n$rd\n+\n$qu\n";
 	}
