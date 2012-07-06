d27e996624482bf0acc50c19e1bbafa0ad5144b8
Robert Theis <robert.m.theis@gmail.com>
2012-07-06 16:59:41 -0700

    fix line numbering

--- trim_clustal.pl
+++ trim_clustal.pl
@@ -19,20 +19,22 @@ use Getopt::Long;
 use IO::File;
 
 #
-# Trims unaligned portions of a clustal multiple sequence alignment output file.
+# Prints a Clustal multiple sequence alignment output file, ignoring unaligned regions.
 #
 # Output is printed to standard out.
 #
 
 my $usage_msg =
-  "Views a clustal multiple sequence alignment output file, ignoring unaligned regions.\n"
-  . "Usage: trim_clustal.pl -i clustal_file.aln {-m margin (default:50)}\n";
+  "Prints a Clustal multiple sequence alignment output file, ignoring unaligned regions.\n"
+  . "Usage: trim_clustal.pl -i clustal_file.aln {--margin [int, default=100]} {--no-num}\n";
 die $usage_msg unless ( @ARGV );
 my $input_file;
-my $margin_size = 50;
+my $margin_size = 100;
+my $suppress_line_numbers = 0;
 GetOptions(
-   "i:s" =>\$input_file,
-   "m:i" =>\$margin_size
+   "i|input=s" =>\$input_file,
+   "m|margin:i" =>\$margin_size, # Length of sequence to show before/after aligned regions
+   "n|no-num" => \$suppress_line_numbers
 ) || die "$0: Bad option";
 die $usage_msg unless ( defined $input_file );
 
@@ -41,54 +43,46 @@ my $first_line = $ifh->getline;
 print STDERR "Warning: first line does not begin with 'CLUSTAL'\n" if $first_line !~ m/^CLUSTAL/;
 $ifh->getline;
 $ifh->getline;
-
-my $pos = 0;
-my $cnt = 0;
 my @buffer = ();
 my $buffer_size = 0;
-my $print_trailing_positions = 0;
+my $cnt = 0;
+my $pos = 0;
+my $trailing_positions = 0;
 while ( my $line = $ifh->getline ) { 
   # Handle a group when we see a blank line or '*' characters indicating aligned positions
   if ($line =~ /^\s*$/ || $line =~ m/\*/) {
-    push(@buffer, $line);
-    push(@buffer, $ifh->getline);
     $pos += 50;
     $buffer_size += 50;
+    push(@buffer, $line);
+    push(@buffer, $ifh->getline);  
     
     # Check for an alignment (two or more sequences are present)
     if ($cnt >= 2) {
-      print "$pos\n";
-      while ( scalar(@buffer) > 0 ) {
-        print shift(@buffer);
-      }
-      $buffer_size = 0;
-      $print_trailing_positions = $margin_size;
-    }
-
-    # Check if this group follows an alignment
-    elsif ($print_trailing_positions > 0) {
       print shift(@buffer) while ( scalar(@buffer) > 0 );
-      $print_trailing_positions -= 50;
+      $buffer_size = 0;
+      $trailing_positions = $margin_size;
+    } elsif ($trailing_positions > 0) { # Check if this group follows an alignment
+      print shift(@buffer) while ( scalar(@buffer) > 0 );
+      $trailing_positions -= 50;
       $buffer_size -= 50;
       $cnt = 0;
+      push(@buffer, "$pos\n") unless $suppress_line_numbers;
       next;
     }    
     $cnt = 0;
     
     # Check if we need to flush a group from the buffer
     if ($buffer_size > $margin_size) {
-      my $buffer_line = "";
       while ( scalar(@buffer) > 0 ) {
-        $buffer_line = shift(@buffer);
-        if ($buffer_line =~ /^\s*$/) {
+        if (shift(@buffer) =~ /^\s*$/) { # A blank line ends the group
           shift(@buffer);
           last;
         }
       }
     }
+    push(@buffer, "$pos\n") unless $suppress_line_numbers; 
   } else {
-    # Add the current non-blank line to the buffer
-    push(@buffer, $line);
+    push(@buffer, $line); # Add the current non-blank line to the buffer
     my @fields = split(/\s+/, $line);
     $cnt++ if ($fields[1] !~ m/^-+$/);
   }
