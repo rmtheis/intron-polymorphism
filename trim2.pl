093ede235e04cd3f872a2341b342297cecb0c636
Robert Theis <robert.m.theis@gmail.com>
2012-11-04 15:04:29 -0800

    fix whitespace in code

--- trim2.pl
+++ trim2.pl
@@ -4,8 +4,7 @@
 #  FOR EACH READ, TRIMS THEM BY THE STRATEGY LAID OUT IN KELLEY ET AL
 #  (PMID: 21114842), EQUATION 1.
 
-($#ARGV == 3) || die "usage: trim2.pl mate1.fq mate2.fq t-score-parameter minimum_output_rea
-d_length\n\n";
+($#ARGV == 3) || die "usage: trim2.pl mate1.fq mate2.fq t-score-parameter minimum_output_read_length\n\n";
 $t = $ARGV[2];
 $min_read_length = $ARGV[3];
 
@@ -23,28 +22,28 @@ open (OUT2, ">$ARGV[1].trimmed");
 
 while (!eof(IN1)) {
     $discard = 0;
-        (@{$data[0]}) = get_fastq(*IN1);
-	    (@{$data[1]}) = get_fastq(*IN2);
-	        #  This is the meet of the algorithm
-		    for $f (0,1) {
-		    	$score = $max = $trim[$f] = 0;
-			       #  Quality scores in reverse order
-			       	  @b = (reverse $data[$f][3] =~ /./g);
-				     #  Go through the quality scores one by one
-				     	for $i (0..$#b) {
-					        #  Increment the score by $t - qual_value
-						       $score += $t - ((ord $b[$i])-66);
+    (@{$data[0]}) = get_fastq(*IN1);
+    (@{$data[1]}) = get_fastq(*IN2);
+    #  This is the meet of the algorithm
+    for $f (0,1) {
+        $score = $max = $trim[$f] = 0;
+        #  Quality scores in reverse order
+        @b = (reverse $data[$f][3] =~ /./g);
+        #  Go through the quality scores one by one
+        for $i (0..$#b) {
+            #  Increment the score by $t - qual_value
+            $score += $t - ((ord $b[$i])-66);
 
-						       	          if ($score > $max) {
-								     $max = $score;
-								     	  $trim[$f] = $i+1;
-									      }
-									          #  If the score is way less than the already-established maximum,
-										      #  don't waste your time.
-										          elsif ($score < $max - 500) {last};
-											  }
-											  #  This variable keeps track of whether either read will be too short
-											  $discard += ($trim[$f] > ((length $data[0][1])-$min_read_length));
+            if ($score > $max) {
+                $max = $score;
+                $trim[$f] = $i+1;
+            }
+            #  If the score is way less than the already-established maximum,
+            #  don't waste your time.
+            elsif ($score < $max - 500) {last};
+        }
+        #  This variable keeps track of whether either read will be too short
+        $discard += ($trim[$f] > ((length $data[0][1])-$min_read_length));
     }
     print "$discard, $trim[0], $trim[1]\n";
     
