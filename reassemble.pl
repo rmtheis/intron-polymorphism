db6bdb958dd8bcd313b116ccdf14f3edd744a118
Robert Theis <robert.m.theis@gmail.com>
2012-11-07 01:03:42 -0800

    add temporary script for rerunning assembly

---
+++ reassemble.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+use strict;
+use IntronPoly;
+
+my $project = IntronPoly->new();
+$project->set_work_dir( "/home/theis/ccmp-reassemble", "/home/theis/intron-polymorphism" );
+$project->build_db(
+  "/home/theis/testdata/MicromonasCCMP1545.fasta",
+  "/home/theis/testdata/CCMP490_7.1.trimmed",
+  "/home/theis/testdata/CCMP490_7.2.trimmed",
+);
+
+$project->set_fragment_length( 280 );
+$project->assemble_groups( 250, 3, 70, "/home/theis/intron-polymorphism/ip_out/run-20121102_16_09_18/CCMP490_7.1_filtered2_sorted.sam" );
+$project->align_groups_clustal( $output_file );
+
+exit;
