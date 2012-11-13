3728bd73ad858d2b8064ab208927e7547394c467
Robert Theis <robert.m.theis@gmail.com>
2012-11-12 18:02:04 -0800

    update rerun script

--- rerun.pl
+++ rerun.pl
@@ -4,7 +4,7 @@ use strict;
 use IntronPoly;
 
 my $project = IntronPoly->new();
-$project->set_work_dir( "/home/theis/ccmp-reassemble", "/home/theis/intron-polymorphism" );
+$project->set_work_dir( "/home/theis/ccmp-rerun", "/home/theis/intron-polymorphism" );
 $project->build_db(
   "/home/theis/testdata/MicromonasCCMP1545.fasta",
   "/home/theis/testdata/CCMP490_7.1.trimmed",
@@ -13,5 +13,5 @@ $project->build_db(
 
 $project->set_fragment_length( 280 );
 #$project->assemble_groups( 250, 3, 70, "/home/theis/intron-polymorphism/ip_out/run-20121102_16_09_18/CCMP490_7.1_filtered2_sorted.sam" );
-$project->align_groups_clustal( "/home/theis/ccmp-reassemble/run-20121107_22_51_55/CCMP490_7.1_contigs.fa" );
+$project->align_contigs_clustal( "/home/theis/ccmp-reassemble/run-20121107_22_51_55/CCMP490_7.1_contigs.fa" );
 
